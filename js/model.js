9f5930e82e1fd28aad2274ae0404d5ad295bb967
Meyrie <marie_esturgie@homail.fr>
2018-09-07 16:33:20 +0200

    Description du code EN / FR

--- js/model.js
+++ js/model.js
@@ -3,9 +3,11 @@
 
 	/**
 	 * Creates a new Model instance and hooks up the storage.
-	 *
+	 * Crée une nouvelle instance de model et relié au stock.
+
 	 * @constructor
-	 * @param {object} storage A reference to the client side storage class
+	 * @param {object} storage A reference to the client side storage class.
+	 * Une référence à la classe de stockage côté client
 	 */
 	function Model(storage) {
 		this.storage = storage;
@@ -13,8 +15,8 @@
 
 	/**
 	 * Creates a new todo model
-	 *
-	 * @param {string} [title] The title of the task
+	 *Crée un nouveau model de todo.
+	 * @param {string} [title] The title of the task. Le titre de la tâche.
 	 * @param {function} [callback] The callback to fire after the model is created
 	 */
 	Model.prototype.create = function (title, callback) {
@@ -34,12 +36,14 @@
 	 * return everything. If you pass in a string or number it'll look that up as
 	 * the ID of the model to find. Lastly, you can pass it an object to match
 	 * against.
-	 *
-	 * @param {string|number|object} [query] A query to match models against
+	 * Trouve et renvoie un modèle en mémoire. Si aucune requête n'est donnée, il va simplement
+	 * tout retourner. Si vous passez une chaîne ou un numéro, cela ressemblera à l'identifiant
+	 * du modèle à trouver. Enfin, vous pouvez lui passer un objet.
+	 * @param {string|number|object} [query] A query to match models against.  Une requête pour faire correspondre les modèles
 	 * @param {function} [callback] The callback to fire after the model is found
 	 *
 	 * @example
-	 * model.read(1, func); // Will find the model with an ID of 1
+	 * model.read(1, func); // Will find the model with an ID of 1. Trouvera le modèle avec un ID de 1
 	 * model.read('1'); // Same as above
 	 * //Below will find a model with foo equalling bar and hello equalling world.
 	 * model.read({ foo: 'bar', hello: 'world' });
@@ -62,9 +66,10 @@
 	/**
 	 * Updates a model by giving it an ID, data to update, and a callback to fire when
 	 * the update is complete.
-	 *
-	 * @param {number} id The id of the model to update
-	 * @param {object} data The properties to update and their new value
+	  *Met à jour un modèle en lui attribuant un ID, des données et un callback lorsque la mise à jour
+	 * est terminée.
+	 * @param {number} id The id of the model to update. L'id du modèle à mettre à jour.
+	 * @param {object} data The properties to update and their new value. les propriétés à mettre à jour et leurs valeurs.
 	 * @param {function} callback The callback to fire when the update is complete.
 	 */
 	Model.prototype.update = function (id, data, callback) {
@@ -73,8 +78,8 @@
 
 	/**
 	 * Removes a model from storage
-	 *
-	 * @param {number} id The ID of the model to remove
+	 * Supprime un modèle du stockage.
+	 * @param {number} id The ID of the model to remove. L'id du modèle à supprimer.
 	 * @param {function} callback The callback to fire when the removal is complete.
 	 */
 	Model.prototype.remove = function (id, callback) {
@@ -92,6 +97,7 @@
 
 	/**
 	 * Returns a count of all todos
+	 * Renvoie tous les todos
 	 */
 	Model.prototype.getCount = function (callback) {
 		var todos = {
